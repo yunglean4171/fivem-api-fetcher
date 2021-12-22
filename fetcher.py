c152219374c3564cbb4e714e6b251056beb0210a
yunglean_ <71585779+yunglean4171@users.noreply.github.com>
2021-12-22 13:47:20 +0100

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -2,44 +2,45 @@ import requests
 import json
 from urllib.request import Request, urlopen
 
-link = input("➤ Provide cfx.re link: ")
-response = requests.get("https://" + link)
 
-try:
-    ip = response.headers['X-Citizenfx-Url']
-    print("IP: " + ip)
-except:
-    print("server blocked ip fetching")
+def main():
+    link = input("➤ Provide cfx.re link: ")
+    response = requests.get("https://" + link)
 
-req = Request('https://servers-frontend.fivem.net/api/servers/single/' + link.replace('cfx.re/join/', ''), headers={'User-Agent': 'Mozilla/5.0'})
-sopen = urlopen(req, timeout=10)
-data = json.loads(sopen.read())
+    try:
+        ip = response.headers['X-Citizenfx-Url']
+        print("IP: " + ip)
+    except:
+        print("server blocked ip fetching")
 
-class values:
-    serversname = "\nServer hostname: " + data['Data']['hostname']
-    gametype = "Gametype: " + data['Data']['gametype']
-    onlineplayers = "\nOnline players: " + str(data['Data']['clients'])
-    maxplayers = str(data['Data']['sv_maxclients'])
-    resources = "\nServer resources: " + str(data['Data']['resources'])
-    cbanner = "\nConnecting banner img path: " + data['Data']['vars']['banner_connecting']
-    dbanner = "Detail banner img path: " + data['Data']['vars']['banner_detail']
-    description = "\nServer description: " + data['Data']['vars']['sv_projectDesc']
-    players = data['Data']['players']
+    req = Request('https://servers-frontend.fivem.net/api/servers/single/' + link.replace('cfx.re/join/', ''),
+                  headers={'User-Agent': 'Mozilla/5.0'})
+    sopen = urlopen(req, timeout=10)
+    data = json.loads(sopen.read())
 
-print(values.serversname)
-print(values.gametype)
-print(values.onlineplayers + " / " + values.maxplayers)
-print(values.resources)
-print(values.cbanner)
-print(values.dbanner)
-print(values.description)
-print("\nPlayers online:\n")
+    class values:
+        serversname = "\nServers hostname: " + data['Data']['hostname']
+        gametype = "Gametype: " + data['Data']['gametype']
+        onlineplayers = "\nOnline players: " + str(data['Data']['clients'])
+        maxplayers = str(data['Data']['sv_maxclients'])
+        resources = "\nServer resources: " + str(data['Data']['resources'])
+        cbanner = "\nConnecting banner img path: " + data['Data']['vars']['banner_connecting']
+        dbanner = "Detail banner img path: " + data['Data']['vars']['banner_detail']
+        description = "\nServer description: " + data['Data']['vars']['sv_projectDesc']
+        players = data['Data']['players']
 
-for name in values.players:
-    print(name['name'])
+    print(values.serversname)
+    print(values.gametype)
+    print(values.onlineplayers + " / " + values.maxplayers)
+    print(values.resources)
+    print(values.cbanner)
+    print(values.dbanner)
+    print(values.description)
+    print("\nPlayers online:\n")
+
+    for name in values.players:
+        print(name['name'])
 
 
-
-
-
-
+if __name__ == "__main__":
+    main()
